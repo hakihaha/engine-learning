819d6b6d6fbc580be997254e90a5fe1ceee45f65
hakihaha <liuboluoyao@163.com>
2025-12-21 23:03:15 +0800

    testcpp的一些修改

--- C++_project/testcpp/testcpp.cpp
+++ C++_project/testcpp/testcpp.cpp
@@ -2,7 +2,7 @@
 #include<bitset>
 #include<windows.h>
 using namespace std;
-int main()
+int main(int agc,char *argv[])
 {
     /*char a = 0b10000001;
     char b = 0b00000001;
@@ -28,7 +28,7 @@ int main()
     }*/
     //测试枚举类型
     //传统枚举类型
-    enum 枚举类型名
+    /*enum 枚举类型名
     {
         枚举值1,
         枚举值2,
@@ -39,5 +39,29 @@ int main()
     };
     枚举类型名 evl{ 枚举值1 };
     cout << "ev1" << evl << endl;
-
+    //现代枚举
+    enum class Log
+    {
+        DEBUG,
+        INFO,
+        FATAL
+    
+    };
+    Log level{ Log::DEBUG };
+    cout << "level =" << (int)level << endl;
+    */
+    //main函数的测试
+    cout << "argc = " << agc << endl;
+    //访问argv字符串数组中的内容
+    //从0开始是第一个字符串
+    cout << argv[0] << endl;
+    //第二个参数是用户传递
+    if (agc >1)
+    {
+        cout << argv[1] << endl;
+    }
+    if (agc >2)
+    {
+        cout << argv[2] << endl;
+    }
 }
