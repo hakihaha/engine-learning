30007e495d59bdc25f1754590f48cff1c5db96d2
hakihaha <liuboluoyao@163.com>
2025-12-30 22:11:37 +0800

    指针工程的创建

---
+++ C++_project/Pointer/Pointer.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+int max(int num1, int num2);
+int main()
+{
+	int a = 100;
+	int b = 200;
+	int ret;
+	ret = max(a,b);
+	cout << "max value is :" << ret << endl;
+	return 0;
+	
+}
+int max(int num1, int num2)
+{
+	int result;
+	if (num1 > num2)
+	{
+		result = num1;
+	}
+	else
+	{
+		result = num2;
+	}
+	return result;
+}
+
+
