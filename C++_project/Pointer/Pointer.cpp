455309ebd175fdab736cada13ef8fff3cc0a65b2
hakihaha <liuboluoyao@163.com>
2025-12-30 22:25:09 +0800

    参数的默认值

--- C++_project/Pointer/Pointer.cpp
+++ C++_project/Pointer/Pointer.cpp
@@ -1,28 +1,24 @@
-﻿#include <iostream>
+﻿//参数的默认值
+#include <iostream>
 using namespace std;
-int max(int num1, int num2);
+int sum(int a, int b = 20)
+{
+	int result;
+	result = a + b;
+	return result;
+}
 int main()
 {
 	int a = 100;
 	int b = 200;
-	int ret;
-	ret = max(a,b);
-	cout << "max value is :" << ret << endl;
-	return 0;
-	
-}
-int max(int num1, int num2)
-{
 	int result;
-	if (num1 > num2)
-	{
-		result = num1;
-	}
-	else
-	{
-		result = num2;
-	}
-	return result;
+	result = sum(a, b);
+	cout << "sum value is:" << result << endl;
+	result = sum(a);
+	cout << "sum value is:" << result << endl;
+	return 0;
 }
+//当参数参数有默认数值时候,有指定的值的时候用的是指定的值，没有指定的值的时候用的默认值
 
+	
 
